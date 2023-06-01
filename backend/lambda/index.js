096e77130a446dbcff1973af1d548e419f83bfd1
Tom <tsnuttall@gmail.com>
2023-06-01 22:16:31 +0100

    backend: Added lambda code to read database

---
+++ backend/lambda/index.js
@@ -0,0 +1,30 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
+
+const client = new DynamoDBClient({})
+const dynamo = DynamoDBDocumentClient.from(client)
+
+export const handler = async (event) => {
+  const query = event.queryStringParameters // Dict
+
+  const res = await dynamo.send(
+    new ScanCommand({ TableName: 'demo-dbtable-iot-backend' }),
+  )
+
+  const items = res.Items.map((x) => {
+    const {
+      time,
+      payload: { temp },
+    } = x
+    const date = new Date(Number(time))
+    return { date, temp }
+  }).sort((a, b) => a.date - b.date)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(items),
+    headers: { 'content-type': 'application/json' },
+  }
+
+  return response
+}
