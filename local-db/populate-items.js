c45eb9f2b6917953f9d9471571e67f8e2ae7b0f1
Tom <tsnuttall@gmail.com>
2023-07-27 21:33:04 +0100

    local-db: Tweak generating local db values to more of a curve

--- local-db/populate-items.js
+++ local-db/populate-items.js
@@ -19,18 +19,34 @@ const random = (max) => {
   return Math.floor(Math.random() * max)
 }
 
+const getTemp = (a, d, i, step) => {
+  const x = i * step
+  const radians = x * (Math.PI / 180)
+  return a * Math.sin(radians) + (d + 2 * Math.random())
+}
+
 const populateTable = async () => {
   const now = startOfHour(new Date())
   const numReadings = 24 * 60
+  const step = 360 / (24 - 1)
+  let a = 0
+  let d = 0
+
   const promises = [...Array(numReadings)].map((_, index) => {
     const timestamp = subHours(now, index)
+
+    if (index % 24 === 0) {
+      a = random(30)
+      d = random(10)
+    }
+
     return client.send(
       new PutItemCommand({
         TableName: table_schema.TableName,
         Item: {
           date: { S: format(timestamp, 'dd/MM/yyyy') },
           time: { N: timestamp.valueOf() },
-          temp: { N: random(30) },
+          temp: { N: getTemp(a, d, index, step) },
         },
       }),
     )
