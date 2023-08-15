39c8eaaceaa002a62eb3e45f39618bd799ffc961
Tom <tsnuttall@gmail.com>
2023-08-15 23:41:09 +0100

    dashboard: Add PWA support

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   test: {
     globals: true,
     environment: 'happy-dom',
